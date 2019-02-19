c6cb58054a6fbd25d95ee955420fb89db51c33f7
blackwell <blackwellsmithiv@gmail.com>
2019-02-19 01:10:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     #binding.pry
-  end  
+    
   
    doc.css(".post").each do |post|
       course = Course.new
